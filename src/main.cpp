f8d32505bdc0b6b0efdb73b2aca7b5f317c8bde6
baudelioandalon <baudelio_andalon@hotmail.com>
2019-08-08 16:47:27 -0500

    S

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,40 @@
 #include <Arduino.h>
 
+String someString = "2019";
+
 void setup() {
-  // put your setup code here, to run once:
+  //Replace instead of whatever you want number
+  if(isNumeric(someString)){
+    Serial.println("TRUE");
+  }else{
+    Serial.println("FALSE");
+  }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
 }
+
+boolean isNumeric(String str) {
+    unsigned int stringLength = str.length();
+ 
+    if (stringLength == 0) {
+        return false;
+    }
+    boolean seenDecimal = false;
+ 
+    for(unsigned int i = 0; i < stringLength; ++i) {
+        if (isDigit(str.charAt(i))) {
+            continue;
+        }
+        if (str.charAt(i) == '.') {
+            if (seenDecimal) {
+                return false;
+            }
+            seenDecimal = true;
+            continue;
+        }
+        return false;
+    }
+    return true;
+}//fin isNumeric
